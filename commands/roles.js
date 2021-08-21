37d855f196d62005392f3677b97a4adc4ce71ed6
Bee <bee@beefox>
2021-08-21 19:52:56 +1000

    whoops

--- commands/roles.js
+++ commands/roles.js
@@ -178,8 +178,6 @@ async function add (interaction) {
     
     }
 
-    BaseGuildEmojiManager.resolveEmoji();
-
     // eslint-disable-next-line require-unicode-regexp
     const emojiRegex = /(?:\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff]|<a?:.+?:\d+>)/i;
 
